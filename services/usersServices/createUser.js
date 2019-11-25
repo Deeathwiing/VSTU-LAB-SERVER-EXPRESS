a7c49b3239e6692473c8111a5e29f504b5b86f26
Deeathwiing <andreykorotkins@gmail.com>
2019-11-26 00:57:18 +0300

    fix relations created automatical adding role for new users

--- services/usersServices/createUser.js
+++ services/usersServices/createUser.js
@@ -2,25 +2,26 @@ import cryptoJs from "crypto-js";
 import { models } from "../../init/dataBaseUtils";
 
 export const createUser = async data => {
-  const result = await models.User.findAll({
-    Where: {
+  const result = await models.User.findOne({
+    where: {
       email: data.email
     }
   });
-
-  if (result == false) {
+  if (result == null) {
     let encryptedPass = cryptoJs.AES.encrypt(
       JSON.stringify(data.password),
       "It's easy 322"
     );
     encryptedPass = encryptedPass.toString();
     try {
-      await models.User.create({
+      const user = await models.User.create({
         email: data.email,
         firstName: data.firstName,
         lastName: data.lastName,
         password: encryptedPass
       });
+      const role = await models.Role.findOne({ where: { userRole: "user" } });
+      await user.addRole(role);
     } catch (err) {
       return 418;
     }
