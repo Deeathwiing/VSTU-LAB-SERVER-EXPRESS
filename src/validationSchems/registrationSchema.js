75085753bfa6dad61f8b324642423bf107e8c1dc
Deeathwiing <andreykorotkins@gmail.com>
2020-01-31 23:52:03 +0300

    added joi schema for registration

---
+++ src/validationSchems/registrationSchema.js
@@ -0,0 +1,52 @@
+const Joi = require("@hapi/joi");
+
+module.exports = Joi.object({
+  email: Joi.string()
+    .email()
+    .required()
+    .min(5)
+    .max(50)
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .messages({
+      "string.base": `Invalid type. Email must be a string`,
+      "string.empty": `Please enter your email`,
+      "any.required": `Email is a required field`
+    }),
+  firstName: Joi.string()
+    .min(3)
+    .max(20)
+    .pattern(new RegExp("/^[a-zA-Z]+$/"))
+    .required()
+    .messages({
+      "string.base": `Invalid type. First name must be a string`,
+      "string.empty": `Please enter your First name`,
+      "string.pattern.base": `First name must consists of letters only`,
+      "string.min": `First name should have a minimum length of {#limit}`,
+      "string.max": `First name should have a maximum length of {#limit}`,
+      "any.required": `First name is a required field`
+    }),
+  lastName: Joi.string()
+    .min(3)
+    .max(30)
+    .pattern(new RegExp("/^[a-zA-Z]+$/"))
+    .required()
+    .messages({
+      "string.base": `Invalid type. Last name must be a string`,
+      "string.empty": `Please enter your last name`,
+      "string.min": `Last name should have a minimum length of {#limit}`,
+      "string.max": `Last name should have a maximum length of {#limit}`,
+      "any.required": `Last name is a required field`
+    }),
+
+  password: Joi.string()
+    .required()
+    .min(6)
+    .max(20)
+    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+    .messages({
+      "string.empty": `Please enter your password`,
+      "string.min": `Password should have a minimum length of {#limit}`,
+      "string.max": `Password should have a maximum length of {#limit}`,
+      "any.required": `Password is a required field`
+    })
+});
